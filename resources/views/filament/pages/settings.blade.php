37299410b00f6b17456d55ce417d15236a6c4abe
Omodamola Oladeji <darmhoo@hotmail.com>
2024-12-22 22:38:08 +0000

    initail commit

---
+++ resources/views/filament/pages/settings.blade.php
@@ -0,0 +1,5 @@
+<x-filament-panels::page>
+    <div>
+        settings
+    </div>
+</x-filament-panels::page>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import preset from './vendor/filament/support/tailwind.config.preset'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    presets: [preset],
+    content: [
+        './app/Filament/**/*.php',
+        './resources/views/filament/**/*.blade.php',
+        './vendor/filament/**/*.blade.php',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+    plugins: [],
+};
