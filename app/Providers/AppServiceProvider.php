eb7b2f844af3970074ca9fc51c5684a52ddeff78
Omodamola Oladeji <darmhoo@hotmail.com>
2024-12-25 06:01:31 +0000

    uop

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Livewire;
 
@@ -25,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
         Livewire::setScriptRoute(function ($handle) {
             return Route::get('/livewire/livewire-js', $handle);
         });
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
