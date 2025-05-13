319fd6d6a5afffad1df4bdd22f41632eafe741d9
Omodamola Oladeji <darmhoo@hotmail.com>
2025-05-13 20:11:52 +0000

    fund wallet

--- app/Http/Controllers/VirtualAccountController.php
+++ app/Http/Controllers/VirtualAccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Transaction;
+use App\Models\User;
 use App\Models\VirtualAccount;
 use Illuminate\Http\Request;
 
@@ -12,24 +13,25 @@ class VirtualAccountController extends Controller
     public function verifyPaymentPV(Request $request)
     {
         // dd($request->all());
-        $payload = $request->getContent();
-        if (empty($payload)) {
+        $data = $request->all();
+        // dd($payload);
+        if (empty($data)) {
             return response()->json([
                 "response" => "Payload is empty"
             ]);
         }
-        $payvessel_signature = $request['HTTP_PAYVESSEL_HTTP_SIGNATURE'];
-        $ip_address = request()->ip();
-        $api_secret = VirtualAccount::where('name', 'Payvessel')->first()->secret_key;
-        $ipWhiteList = ['3.225.23.38'];
-        $hash_key = hash_hmac('sha256', $payload, $api_secret);
-        if ($payvessel_signature !== $hash_key || !in_array($ip_address, $ipWhiteList)) {
-            return response()->json([
-                "response" => "Permission denied, invalid hash or ip address."
-            ]);
-        }
+        // $payvessel_signature = $request['HTTP_PAYVESSEL_HTTP_SIGNATURE'];
+        // $ip_address = request()->ip();
+        // $api_secret = VirtualAccount::where('name', 'Payvessel')->first()->secret_key;
+        // $ipWhiteList = ['3.225.23.38'];
+        // $hash_key = hash_hmac('sha256', $payload, $api_secret);
+        // if ($payvessel_signature !== $hash_key || !in_array($ip_address, $ipWhiteList)) {
+        //     return response()->json([
+        //         "response" => "Permission denied, invalid hash or ip address."
+        //     ]);
+        // }
 
-        $data = json_decode($payload, true);
+        // $data = json_decode($payload, true);
         $amount = $data['order']['amount'];
         $settlementAmount = $data['order']['settlement_amount'];
         $fee = $data['order']['fee'];
@@ -44,17 +46,20 @@ class VirtualAccountController extends Controller
             ], 200);
         }
 
-        // $transaction = Transaction::create([
-        //     'user_id' => $request['user_id'],
-        //     'amount' => $amount,
-        //     'settlement_amount' => $settlementAmount,
-        //     'fee' => $fee,
-        //     'reference' => $reference,
-        //     'description' => $description,
-        //     'status' => 'pending',
-        //     'type' => 'credit',
-        //     'method' => 'virtual_account'
-        // ]);
+        $user = User::where('email', $data['customer']['email'])->first();
+        $user->deposit($settlementAmount);
+
+        $transaction = Transaction::create([
+            'user_id' => $user->id,
+            'price' => $settlementAmount,
+            'amount_before' => $user->balance - $settlementAmount,
+            'amount_after' => $user->balance,
+            'reference' => $reference,
+            'description' => $description,
+            'status' => 'completed',
+            'transaction_type' => 'fund-wallet',
+            'api_message' => 'success',
+        ]);
         // if ($transaction && $request['data']['content']['transactions']['status']) {
         //     $transaction->update([
         //         'status' => 'completed',
